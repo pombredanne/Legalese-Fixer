74df85f0e493625dff7cdecfaa94e474fc2b6de1
Joe Pires <piresjo@seas.upenn.edu>
2017-04-22 18:22:21 -0400

    Added Trie For Spellchecking

--- basicLegaleseFixer.py
+++ basicLegaleseFixer.py
@@ -10,6 +10,72 @@ exampleList = []
 fixList = []
 dictList = []
 
+''' Trie used for spellchecking 
+	based off notes from CIS 121'''
+
+class Node:    
+    def __init__(self):
+        self.word = None
+        self.nodes = {} # dictionary of various nodes
+        
+    def __getAll__(self):
+    	''' Recursively get all the words in the trie '''
+        x = []
+        for key, node in self.nodes.iteritems() : 
+            if(node.word is not None):
+                x.append(node.word)
+            x += node.__getAll__()      
+        return x
+    
+    def __str__(self):
+        return self.word
+    
+    def __insert__(self, word, stringPos = 0):
+        currentLetter = word[stringPos]
+        
+        # Create the Node if it does not already exist
+        if current_letter not in self.nodes:
+            self.nodes[currentLetter] = Node();
+        if(string_pos + 1 == len(word)):
+            self.nodes[currentLetter].word = word
+        else:
+        	self.nodes[currentLetter].__insert__(word, stringPos + 1)   
+    	return True
+    
+    def __getAllPrefix__(self, prefix, stringPos):
+        ''' Much like getAll, but with a prefix '''
+        x = []
+        for key, node in self.nodes.iteritems() : 
+            # If the current character of the prefix is one of the nodes or we have
+            # already satisfied the prefix match, then get the matches
+            if(stringPos >= len(prefix) or key == prefix[stringPos]):
+            	if(node.word is not None):
+                	x.append(node.word)
+                    
+                if(node.nodes != {}):
+                    if(string_pos + 1 <= len(prefix)):
+                        x += node.__getAllPrefix__(prefix, stringPos + 1)
+                    else:
+            			x += node.__getAllPrefix__(prefix, stringPos)
+    
+        return x       
+
+
+class Trie:
+   def __init__(self):
+        self.root = Node()
+        
+   def insert(self, word):
+        self.root.__insert__(word)
+        
+   def getAll(self):
+        return self.root.__getAll__()
+
+   def getAllPrefix(self, prefix, stringPos = 0):
+        return self.root.__getAllPrefix__(prefix, stringPos)
+
+
+
 def checkAndEditLegalese(fileString):
 	fileStringList = fileString.split()
 	for i in range (0, len(fileStringList)):
@@ -73,6 +139,7 @@ def main(argv):
 	return 0
 
 	fileText = fileToExamine.read()
+	global fileText
 
 	''' Now close the files '''
 	dictFile.close()
